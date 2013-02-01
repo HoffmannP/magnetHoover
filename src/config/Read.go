6b7430705ca4eeee97270a84a1db39c0733c5493
Peter Hoffmann <Hoffmann.P@gmx.net>
2013-02-01 17:50:17 +0100

    So wenig für heute

--- src/config/Read.go
+++ src/config/Read.go
@@ -1,34 +1,42 @@
 package config
 
 import (
+	"os"
+	"encoding/json"
 	"flag"
+	"time"
+	"strings"
 )
 
 var configFile = "config.json"
 
 type Config struct {
-	Intervall Intervall
-	Urls      Urls
+	Intervall time.Duration
+	Urls      []string
 }
 
-func Default() {
-	return Config{
-		DefaultIntervall(),
-		nil
-	}
+func (c *Config) Tic() <-chan time.Time {
+	return time.After(c.Intervall)
 }
 
-func FromCmdl(config* Config) (fromFile bool) {
-	config = Default()
-	flag.BoolVar(&fromFile, "r", true, "read config file")
-	flag.StringVar(&configFile, "c", "config file")
-	flag.Var(&config.Intervall, "i", "intervall between to polls")
-	flag.Var(&config.Urls, "u", "url to poll")
+func FromCmdl(config* Config) error {
+	var notFromFile bool
+	var url string
+	
+	flag.BoolVar(&notFromFile, "n", false, "don't read config file")
+	flag.BoolVar(&notFromFile, "noread", false, "don't read config file")
+	flag.StringVar(&configFile, "c", configFile, "config file")
+	flag.StringVar(&configFile, "config", configFile, "config file")
+	flag.DurationVar(&config.Intervall, "i", 5*time.Minute, "intervall between to polls")
+	flag.DurationVar(&config.Intervall, "intervall", 5*time.Minute, "intervall between to polls")
+	flag.StringVar(&url, "u", "", "url to poll")
+	flag.StringVar(&url, "url", "", "url to poll")
 	flag.Parse()
-	if config.Intervall == 0 {
-		config.Intervall = DefaultIntervall()
+	config.Urls = strings.Split(url, ",")
+	if notFromFile {
+		return nil
 	}
-	return
+	return FromFile(config)
 }
 
 func FromFile(config* Config) error {
@@ -43,4 +51,3 @@ func FromFile(config* Config) error {
 	}
 	return nil
 }
-
--- src/magnetHoover.go
+++ src/magnetHoover.go
@@ -2,37 +2,53 @@ package main
 
 import (
 	"config"
-	"encoding/json"
+	"os/signal"
+	"os"
+	"syscall"
+	"fmt"
+	"net/http"
 )
 
-var config Config.config
-
-const SIGKILL = 9
-const SIGHUP = 5
-
-var signal chan byte
-
 func main() {
-	readFile := config.FromCmdl(&config)
-	if  readFile {
-		config.FromCmdl(&config)
-	}
+	var cfg config.Config
+	config.FromCmdl(&cfg)
+	
+	sig := make(chan os.Signal)
+	signal.Notify(sig)
+
 	run := true
+	poll_all(cfg.Urls)
 	for run {
 		select {
-		case <- config.Intervall.After():
-			poll()
-		case s := <-signal:
-			switch s {
-			case SIGKILL:
+		case <-cfg.Tic():
+			poll_all(cfg.Urls)
+		case si := <-sig:
+			switch si {
+			case syscall.SIGINT:
 				run = false
-			case SIGHUP:
-				readConfigFromFile()
+			case syscall.SIGHUP:
+				fmt.Println("Rereading config file")
+				config.FromFile(&cfg)
 			}
 		}
 	}
 }
 
-func poll() {
+func poll_all(urls []string) {
+	if len(urls) == 0 {
+		return
+	}
+	for _, url := range urls {
+		go poll(url)
+	}
+	return
+}
 
+func poll(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+	fmt.Println(response, response.ContentLength)
 }
